a7458144d82fee7b8aa285caa04e1d9c98072b91
cocarls <carloscoronadouribe@gmail.com>
2023-02-01 10:15:54 -0500

    mi tarea

---
+++ siu.js
@@ -0,0 +1,29 @@
+const lista = [
+ nombre  = "carlos alberto coronado uribee",
+  
+ edad = 18,
+ 
+desarrollador= true,
+ 
+
+
+ fecha = new Date("december 22 2004"),
+
+
+
+mi_libro_favorito = {
+    titulo : "el principito",
+    autor : " Antoine de Saint-Exupéry",
+    fecha : "1943",
+    URL : "http://biblio3.url.edu.gt/Libros/2011/el_princito.pdf"
+
+},
+]
+ 
+
+
+
+
+
+
+
